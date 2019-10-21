e40cf3a2901c05152669e6d2d53c7fab37806e4c
Melissa Sedgwick <melissa.sedgwick@yahoo.co.uk>
2019-10-21 15:54:40 +0100

    Initial commit - set up create react app project

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div style={{ marginTop: '10px' }}>
+        <h1>Survey Maker</h1>
+      </div>
+    );
+  }
+}
+
+export default App
